674e70d37f79d6cbf0757d4d1fbd22a2f6a367f1
Your Name <dgebuadzeirakli28@gmail.com>
2025-09-17 11:31:30 +0400

    Making Inner section

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="flex flex-col items-center  w-full min-w-[320px]">
       <Home />
-      <div>hello</div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Heading from "../components/Heading";
 import Comparison from "../components/Comparison";
+import InnerSection from "../components/InnerSection";
 function Home() {
   return (
     <div className="flex flex-col items-center w-full ">
       <Heading />
+      <InnerSection />
       <Comparison />
     </div>
   );
